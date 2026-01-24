c01f056eab724a0648149fb9d07063bdc280ee45
IaK3lwin <krochaalmeida7@gmail.com>
2026-01-24 09:48:50 -0300

    config keys

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=15" };
+static const char dmenufont[]       = "monospace:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -31,6 +31,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  "Toolkit",  NULL,       1,            1,           -1 },
+	{ NULL,       NULL,       "Picture-in-Picture", 0,  1,           -1 },
 };
 
 /* layout(s) */
@@ -61,9 +63,9 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-/*static char dmenumon[2] = "0";
+static char dmenumon[2] = "0";
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-*/
+
 static const char *termcmd[]  = { "kitty", NULL };
 
 /* My commands */
@@ -73,8 +75,9 @@ static const char *flameshot[]  = { "flameshot", "gui", NULL };
 static const char *powermenu[]  = { "/bin/powermenu.sh", NULL };
 
 static const Key keys[] = {
+	
 	/* modifier                     key        function        argument */
-	{ MODKEY|ShiftMask|,            XK_c,      spawn,          {.v = code } },
+	{ MODKEY,                       XK_c,      spawn,          {.v = code } },
 	{ MODKEY,                       XK_s,      spawn,          {.v = flameshot } },
 	{ MODKEY|ShiftMask,             XK_q,      spawn,          {.v = powermenu } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = rofi } },
